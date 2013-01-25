af9cea2dba3ec96f16bf76229116144ddff25778
Voidious <voidious@gmail.com>
2013-01-24 23:24:39 -0500

    add build and run batch files for Windows

---
+++ scripts/bb_windows.bat
@@ -0,0 +1,3 @@
+SET path=%PATH%;.\sfml-lib;.\luajit-lib
+SHIFT
+bbmain.exe %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ scripts/build_windows.bat
@@ -0,0 +1,12 @@
+cd luajit
+make
+cd ..
+g++ bbsfmlmain.cpp bbutil.cpp gfxmanager.cpp filemanager.cpp circle2d.cpp ^
+line2d.cpp point2d.cpp sensorhandler.cpp zone.cpp bbengine.cpp bblua.cpp ^
+gfxeventhandler.cpp rectangle.cpp stage.cpp cliprinthandler.cpp ^
+./luajit/src/lua51.dll -I./luajit/src -I./stlsoft-1.9.116/include ^
+-I./sfml/include -L./sfml-lib -lsfml-graphics -lsfml-window -lsfml-system ^
+-o bbmain
+mkdir luajit-lib
+copy .\luajit\src\lua51.dll .\luajit-lib
+copy .\scripts\bb_windows.bat .\berrybots.bat
