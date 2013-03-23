f91ef5065f36cf102dba639ed7c446ed0b4c797b
Voidious <voidious@gmail.com>
2013-03-23 16:52:46 -0400

    fix XCode dylib paths script

--- scripts/xcode_fix_dylib_paths.sh
+++ scripts/xcode_fix_dylib_paths.sh
@@ -1,22 +1,60 @@
 #!/bin/bash
-# Thanks ynniv http://ynniv.com/blog/2006/02/deploying-app-that-use-dylibs-on-mac.html
+# This script is used for compiling BerryBots on Mac OS X with XCode. For
+# instructions, see:
+# http://berrybots.com/wiki/Compiling_BerryBots_app_on_Mac_OS_X_with_XCode
+#
+# Thanks ynniv for this article:
+# http://ynniv.com/blog/2006/02/deploying-app-that-use-dylibs-on-mac.html
 
 EXECFILE=${BUILT_PRODUCTS_DIR}/${EXECUTABLE_PATH}
 LIBPATH=${BUILT_PRODUCTS_DIR}/${SHARED_SUPPORT_FOLDER_PATH}
-NEWLIBPATH="@executable_path/../SharedSupport"
 
-# space separated list of libraries
-TARGETS="libsfml-network.2.0.dylib libsfml-audio.2.0.dylib libsfml-system.2.0.dylib libsfml-graphics.2.0.dylib libsfml-window.2.0.dylib libwx_baseu-2.9.4.0.0.dylib libwx_baseu_net-2.9.4.0.0.dylib libwx_baseu_xml-2.9.4.0.0.dylib libwx_osx_cocoau_adv-2.9.4.0.0.dylib libwx_osx_cocoau_aui-2.9.4.0.0.dylib libwx_osx_cocoau_core-2.9.4.0.0.dylib libwx_osx_cocoau_gl-2.9.4.0.0.dylib libwx_osx_cocoau_html-2.9.4.0.0.dylib libwx_osx_cocoau_media-2.9.4.0.0.dylib libwx_osx_cocoau_propgrid-2.9.4.0.0.dylib libwx_osx_cocoau_qa-2.9.4.0.0.dylib libwx_osx_cocoau_ribbon-2.9.4.0.0.dylib libwx_osx_cocoau_richtext-2.9.4.0.0.dylib libwx_osx_cocoau_stc-2.9.4.0.0.dylib libwx_osx_cocoau_webview-2.9.4.0.0.dylib libwx_osx_cocoau_xrc-2.9.4.0.0.dylib"
-for TARGET in ${TARGETS} ; do
-  LIBFILE=${LIBPATH}/${TARGET}
-  TARGETID=`otool -DX ${LIBPATH}/$TARGET`
-  NEWTARGETID=${NEWLIBPATH}/${TARGET}
+EXEC_FRAMEWORK_PATH="@executable_path/../Frameworks"
+EXEC_SHARED_LIBPATH="@executable_path/../SharedSupport"
+SFML_LIBNAMES="libsfml-system libsfml-graphics libsfml-window libsfml-audio libsfml-network"
+WX_LIBNAMES="libwx_baseu-2.9 libwx_baseu_net-2.9 libwx_baseu_xml-2.9 libwx_osx_cocoau_adv-2.9 libwx_osx_cocoau_aui-2.9 libwx_osx_cocoau_core-2.9 libwx_osx_cocoau_gl-2.9 libwx_osx_cocoau_html-2.9 libwx_osx_cocoau_media-2.9 libwx_osx_cocoau_propgrid-2.9 libwx_osx_cocoau_qa-2.9 libwx_osx_cocoau_ribbon-2.9 libwx_osx_cocoau_richtext-2.9 libwx_osx_cocoau_stc-2.9 libwx_osx_cocoau_webview-2.9 libwx_osx_cocoau_xrc-2.9"
+
+for TARGET in ${SFML_LIBNAMES} ; do
+  cd ${LIBPATH}
+  if [ -e ${TARGET}.2.dylib ]; then
+    rm ${TARGET}.2.dylib
+  fi
+  if [ -e ${TARGET}.dylib ]; then
+    rm ${TARGET}.dylib
+  fi
+  ln -s ${TARGET}.2.0.dylib ${TARGET}.2.dylib
+  ln -s ${TARGET}.2.0.dylib ${TARGET}.dylib
+  LIBFILE=${LIBPATH}/${TARGET}.2.0.dylib
+  NEWTARGETID=${EXEC_SHARED_LIBPATH}/${TARGET}.2.0.dylib
   install_name_tool -id ${NEWTARGETID} ${LIBFILE}
-  install_name_tool -change ${TARGETID} ${NEWTARGETID} ${EXECFILE}
-  for TARGET2 in ${TARGETS} ; do
-    LIBFILE2=${LIBPATH}/${TARGET2}
-    install_name_tool -change ${TARGETID} ${NEWTARGETID} ${LIBFILE2}
+  for TARGET2 in ${SFML_LIBNAMES} ; do
+    LIBFILE2=${LIBPATH}/${TARGET2}.2.0.dylib
+    install_name_tool -change ${EXEC_FRAMEWORK_PATH}/${TARGET}.2.0.dylib ${NEWTARGETID} ${LIBFILE2}
+    install_name_tool -change ${EXEC_FRAMEWORK_PATH}/${TARGET}.2.dylib ${NEWTARGETID} ${LIBFILE2}
+    install_name_tool -change ${EXEC_FRAMEWORK_PATH}/${TARGET}.dylib ${NEWTARGETID} ${LIBFILE2}
+    install_name_tool -change ${EXEC_FRAMEWORK_PATH}/${TARGET}.2.0.dylib ${NEWTARGETID} ${EXECFILE}
+    install_name_tool -change ${EXEC_FRAMEWORK_PATH}/${TARGET}.2.dylib ${NEWTARGETID} ${EXECFILE}
+    install_name_tool -change ${EXEC_FRAMEWORK_PATH}/${TARGET}.dylib ${NEWTARGETID} ${EXECFILE}
   done
 done
 
-# TODO: create symlinks here so we can use this copy for the library search path
+for TARGET in ${WX_LIBNAMES} ; do
+  cd ${LIBPATH}
+  if [ -e ${TARGET}.4.dylib ]; then
+    rm ${TARGET}.4.dylib
+  fi
+  if [ -e ${TARGET}.dylib ]; then
+    rm ${TARGET}.dylib
+  fi
+  ln -s ${TARGET}.4.0.0.dylib ${TARGET}.4.dylib
+  ln -s ${TARGET}.4.0.0.dylib ${TARGET}.dylib
+  LIBFILE=${LIBPATH}/${TARGET}.4.0.0.dylib
+  TARGETID=`otool -DX ${LIBFILE}`
+  NEWTARGETID=${EXEC_SHARED_LIBPATH}/${TARGET}.4.0.0.dylib
+  install_name_tool -id ${NEWTARGETID} ${LIBFILE}
+  install_name_tool -change ${TARGETID} ${NEWTARGETID} ${EXECFILE}
+  for TARGET2 in ${WX_LIBNAMES} ; do
+    LIBFILE2=${LIBPATH}/${TARGET2}.4.0.0.dylib
+    install_name_tool -change ${TARGETID} ${NEWTARGETID} ${LIBFILE2}
+  done
+done
