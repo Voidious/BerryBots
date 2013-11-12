faa35d1b571b7a589f9d50539dba636514e01ed8
Voidious <voidious@gmail.com>
2013-11-11 22:59:30 -0500

    run match from web UI with parameters set via form

--- cloud/runmatch.pl
+++ cloud/runmatch.pl
@@ -1,11 +1,55 @@
 #!/usr/bin/perl
+use CGI;
+
+@stages = ('battle1.lua', 'joust.lua');
+@opponents = ('chaser.lua', 'jouster.lua', 'randombot.lua', 'wallhugger.lua',
+              'basicbattler.lua');
+
+$query = new CGI;
+
+$stage = $query->param("stage");
+$opponent = $query->param("opponent");
+
+print "Content-type: text/html\n\n";
+
+if (!isValidStage($stage)) {
+  die("Invalid stage: " + $stage . "\n");
+}
+
+if (!isValidOpponent($opponent)) {
+  die("Invalid opponent: " + $opponent . "\n");
+}
 
 chdir('/home/admin/berrybots');
-$s = `/home/admin/berrybots/berrybots stages/sample/arcadeshooter.lua bots/sample/shooterbot.lua`;
+$s = `/home/admin/berrybots/berrybots stages/sample/$stage bots/sample/floatingduck.lua bots/sample/$opponent`;
 $s =~ /\nSaved replay to: replays\/(.*)\n/;
 $replayFilename = $1;
 
 `cp /home/admin/berrybots/replays/$replayFilename /var/www/replays`;
 
-print "Content-type: text/html\n\n";
 print "<?xml version=\"1.0\" ?>\n<r>" . $replayFilename . "</r>\n";
+
+
+sub isValidStage {
+  my $stage = $_[0];
+  my $valid = 0;
+  foreach my $validStage (@stages) {
+    if ($stage == $validStage) {
+      $valid = 1;
+      break;
+    }
+  }
+  return $valid;
+}
+
+sub isValidOpponent {
+  my $opponent = $_[0];
+  my $valid = 0;
+  foreach my $validOpponent (@opponents) {
+    if ($opponent == $validOpponent) {
+      $valid = 1;
+      break;
+    }
+  }
+  return $valid;
+}
