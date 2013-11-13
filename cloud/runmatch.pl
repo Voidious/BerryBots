bbafad33cb2cb4ca3440cf6791d127f17d175d75
Voidious <voidious@gmail.com>
2013-11-12 22:48:17 -0500

    hook editor to cloud backend, fully functional web UI

--- cloud/runmatch.pl
+++ cloud/runmatch.pl
@@ -3,14 +3,20 @@ use CGI;
 
 $basedir = "/home/ubuntu/berrybots";
 
-@stages = ('battle1.lua', 'joust.lua');
+@stages = ('battle1.lua', 'joust.lua', 'maze2.lua');
 @opponents = ('chaser.lua', 'jouster.lua', 'randombot.lua', 'wallhugger.lua',
-              'basicbattler.lua');
+              'basicbattler.lua', 'floatingduck.lua', '<none>');
 
 $query = new CGI;
 
 $stage = $query->param("stage");
 $opponent = $query->param("opponent");
+$code = $query->param("code");
+
+$filename = int(rand(100000000)) . ".lua";
+open (OUTFILE, ">" . $basedir . "/bots/" . $filename);
+print (OUTFILE $code);
+close(OUTFILE);
 
 print "Content-type: text/html\n\n";
 
@@ -22,10 +28,21 @@ if (!isValidOpponent($opponent)) {
   die("Invalid opponent: " . $opponent . "\n");
 }
 
+$opponent = "sample/" . $opponent;
+if ($opponent eq "sample/basicbattler.lua") {
+  $opponent = "super" . $opponent;
+}
+if ($opponent =~ /<none>/) {
+  $opponent = "";
+} else {
+  $opponent = " bots/" . $opponent;
+}
+
 chdir($basedir);
-$s = `$basedir/berrybots stages/sample/$stage bots/sample/floatingduck.lua bots/sample/$opponent`;
+$s = `$basedir/berrybots stages/sample/$stage bots/$filename$opponent`;
 $s =~ /\nSaved replay to: replays\/(.*)\n/;
 $replayFilename = $1;
+unlink($basedir . "/bots/" . $filename);
 
 `cp $basedir/replays/$replayFilename /var/www/replays`;
 
