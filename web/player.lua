54f09a6d39a32d82bbbb3585b15eca6c33564fa2
Voidious <voidious@gmail.com>
2013-11-23 17:44:26 -0500

    use textarea for code editor on mobile platforms, load default code from player.lua instead of defining it inline

---
+++ web/player.lua
@@ -0,0 +1,28 @@
+-- A ship control program.
+
+ship = nil
+world = nil
+heading = nil
+force = nil
+
+function init(shipArg, worldArg)
+  ship = shipArg
+  world = worldArg
+
+  ship:setName("Player")
+  ship:setLaserColor(255, 255, 0)
+  heading = 0
+  force = world:constants().MAX_THRUSTER_FORCE / 4
+end
+
+function run(enemyShips, sensors)
+  ship:fireThruster(heading, force)
+end
+
+function roundOver()
+
+end
+
+function gameOver()
+
+end
