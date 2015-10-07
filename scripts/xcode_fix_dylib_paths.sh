b46a8f55acd229231b8708cdb43fa55effb35498
Voidious <voidious@gmail.com>
2015-10-06 22:46:01 -0400

    OS X build fix: link SFML libs to app bundle copy of freetype.framework

--- scripts/xcode_fix_dylib_paths.sh
+++ scripts/xcode_fix_dylib_paths.sh
@@ -9,14 +9,17 @@
 EXECFILE=${BUILT_PRODUCTS_DIR}/${EXECUTABLE_PATH}
 LIBPATH=${BUILT_PRODUCTS_DIR}/${SHARED_SUPPORT_FOLDER_PATH}
 
-EXEC_FRAMEWORK_PATH="@rpath"
+RPATH="@rpath"
 EXEC_SHARED_LIBPATH="@executable_path/../SharedSupport"
+EXEC_FRAMEWORK_PATH="@executable_path/../Frameworks"
 SFML_LIBNAMES="libsfml-system libsfml-graphics libsfml-window libsfml-audio libsfml-network"
 SFML_VERSION="2.3.2"
 SFML_MAJOR="2.3"
 WX_VERSION="0.2.0"
 WX_MAJOR="0"
 WX_LIBNAMES="libwx_baseu-3.0 libwx_baseu_net-3.0 libwx_baseu_xml-3.0 libwx_osx_cocoau_adv-3.0 libwx_osx_cocoau_aui-3.0 libwx_osx_cocoau_core-3.0 libwx_osx_cocoau_gl-3.0 libwx_osx_cocoau_html-3.0 libwx_osx_cocoau_media-3.0 libwx_osx_cocoau_propgrid-3.0 libwx_osx_cocoau_qa-3.0 libwx_osx_cocoau_ribbon-3.0 libwx_osx_cocoau_richtext-3.0 libwx_osx_cocoau_stc-3.0 libwx_osx_cocoau_webview-3.0 libwx_osx_cocoau_xrc-3.0"
+FREETYPE_RPATH=${RPATH}/../Frameworks/freetype.framework/Versions/A/freetype
+FREETYPE_BUNDLE_PATH=${EXEC_FRAMEWORK_PATH}/freetype.framework/Versions/A/freetype
 
 for TARGET in ${SFML_LIBNAMES} ; do
   cd ${LIBPATH}
@@ -33,10 +36,11 @@ for TARGET in ${SFML_LIBNAMES} ; do
   install_name_tool -id ${NEWTARGETID} ${LIBFILE}
   for TARGET2 in ${SFML_LIBNAMES} ; do
     LIBFILE2=${LIBPATH}/${TARGET2}.${SFML_VERSION}.dylib
-    install_name_tool -change ${EXEC_FRAMEWORK_PATH}/${TARGET}.${SFML_VERSION}.dylib ${NEWTARGETID} ${LIBFILE2}
-    install_name_tool -change ${EXEC_FRAMEWORK_PATH}/${TARGET}.${SFML_MAJOR}.dylib ${NEWTARGETID} ${LIBFILE2}
-    install_name_tool -change ${EXEC_FRAMEWORK_PATH}/${TARGET}.dylib ${NEWTARGETID} ${LIBFILE2}
+    install_name_tool -change ${RPATH}/${TARGET}.${SFML_VERSION}.dylib ${NEWTARGETID} ${LIBFILE2}
+    install_name_tool -change ${RPATH}/${TARGET}.${SFML_MAJOR}.dylib ${NEWTARGETID} ${LIBFILE2}
+    install_name_tool -change ${RPATH}/${TARGET}.dylib ${NEWTARGETID} ${LIBFILE2}
   done
+  install_name_tool -change ${FREETYPE_RPATH} ${FREETYPE_BUNDLE_PATH} ${LIBFILE}
 done
 
 for TARGET in ${WX_LIBNAMES} ; do
